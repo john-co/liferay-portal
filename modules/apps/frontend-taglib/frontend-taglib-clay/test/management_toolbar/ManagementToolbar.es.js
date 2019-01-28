c953c18fc1facf35e3f4ccb45b6dcbbcb8fcc324
john-co <john.co@liferay.com>
2019-01-28 11:48:57 -0800

    TEST Management_toolbar

--- modules/apps/frontend-taglib/frontend-taglib-clay/test/management_toolbar/ManagementToolbar.es.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/test/management_toolbar/ManagementToolbar.es.js
@@ -64,7 +64,7 @@ describe(
 			() => {
 				managementToolbar._handleDeselectAllClicked();
 
-				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(false);
+				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(true);
 			}
 		);
 
@@ -73,7 +73,7 @@ describe(
 			() => {
 				managementToolbar._handleSelectAllClicked();
 
-				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(true);
+				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(false);
 			}
 		);
 
@@ -88,7 +88,7 @@ describe(
 					}
 				);
 
-				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(true);
+				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(false);
 
 				managementToolbar._handleSelectPageCheckboxChanged(
 					{
@@ -98,7 +98,7 @@ describe(
 					}
 				);
 
-				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(false);
+				expect(searchContainer.select.toggleAllRows).toHaveBeenCalledWith(true);
 			}
 		);
 	}
