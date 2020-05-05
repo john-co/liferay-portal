56ebfd3f0b7e6a5019e0d59954fd894615c622ee
Chema Balsas <jose.balsas@liferay.com>
2020-05-05 10:47:16 +0200

    LPS-110927 Creates frontend-js-instrumentation-test to track globally fired Liferay Events

---
+++ modules/apps/frontend-js/frontend-js-test-instrumentation/src/main/java/com/liferay/frontend/js/test/instrumentation/internal/servlet/taglib/LiferayEventsInstrumentationDynamicInclude.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.test.instrumentation.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Chema Balsas
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class LiferayEventsInstrumentationDynamicInclude
+	extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		AbsolutePortalURLBuilder absolutePortalURLBuilder =
+			_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
+				httpServletRequest);
+
+		for (String fileName : _FILE_NAMES) {
+			printWriter.print("<script data-senna-track=\"permanent\" src=\"");
+
+			printWriter.print(
+				absolutePortalURLBuilder.forModule(
+					_bundleContext.getBundle(), fileName
+				).build());
+
+			printWriter.println("\" type=\"text/javascript\"></script>");
+		}
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#post");
+	}
+
+	@Activate
+	@Modified
+	protected void activate(
+			BundleContext bundleContext, ComponentContext componentContext,
+			Map<String, Object> properties)
+		throws Exception {
+
+		_bundleContext = bundleContext;
+	}
+
+	private static final String[] _FILE_NAMES = {"events.js"};
+
+	@Reference
+	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
+
+	private BundleContext _bundleContext;
+
+}
