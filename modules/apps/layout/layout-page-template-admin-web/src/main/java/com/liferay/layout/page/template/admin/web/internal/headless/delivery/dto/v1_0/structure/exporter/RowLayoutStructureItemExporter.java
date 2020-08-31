784fd9e2cf06dc84d9d67e0f9aab3376f8d7be3d
John Co <john.co@liferay.com>
2020-08-31 16:25:06 -0700

    Revert "LPS-119156 Sort"
    This reverts commit 988d391cfc95098f149ca5b5f4339687501a48f5.

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/exporter/RowLayoutStructureItemExporter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/exporter/RowLayoutStructureItemExporter.java
@@ -62,26 +62,6 @@ public class RowLayoutStructureItemExporter
 						reverseOrder = getReverseOrder();
 						verticalAlignment = getVerticalAlignment();
 
-						setFragmentStyle(
-							() -> {
-								JSONObject itemConfigJSONObject =
-									rowStyledLayoutStructureItem.
-										getItemConfigJSONObject();
-
-								return toFragmentStyle(
-									itemConfigJSONObject.getJSONObject(
-										"styles"),
-									saveMappingConfiguration);
-							});
-						setFragmentViewports(
-							() -> {
-								JSONObject itemConfigJSONObject =
-									rowStyledLayoutStructureItem.
-										getItemConfigJSONObject();
-
-								return getFragmentViewPorts(
-									itemConfigJSONObject);
-							});
 						setRowViewports(
 							() -> {
 								Map<String, JSONObject>
@@ -116,6 +96,28 @@ public class RowLayoutStructureItemExporter
 									};
 
 								return rowViewports.toArray(new RowViewport[0]);
+							});
+
+						setFragmentStyle(
+							() -> {
+								JSONObject itemConfigJSONObject =
+									rowStyledLayoutStructureItem.
+										getItemConfigJSONObject();
+
+								return toFragmentStyle(
+									itemConfigJSONObject.getJSONObject(
+										"styles"),
+									saveMappingConfiguration);
+							});
+
+						setFragmentViewports(
+							() -> {
+								JSONObject itemConfigJSONObject =
+									rowStyledLayoutStructureItem.
+										getItemConfigJSONObject();
+
+								return getFragmentViewPorts(
+									itemConfigJSONObject);
 							});
 					}
 				};
