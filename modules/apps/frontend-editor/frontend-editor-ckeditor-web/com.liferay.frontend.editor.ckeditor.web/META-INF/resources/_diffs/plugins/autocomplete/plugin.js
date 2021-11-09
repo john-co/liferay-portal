7d8a1ebd91c81e6291fadfa48fbd9dddf9752836
John Co <john.co@liferay.com>
2021-11-08 17:14:03 -0800

    LPS-139942 Run gw deploy -Pnodejs.node.env=test on LPS-139942 commits

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/com.liferay.frontend.editor.ckeditor.web/META-INF/resources/_diffs/plugins/autocomplete/plugin.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/com.liferay.frontend.editor.ckeditor.web/META-INF/resources/_diffs/plugins/autocomplete/plugin.js
@@ -1,2 +1,2 @@
-CKEDITOR.plugins.add("autocomplete",{init(e){var t=this;AUI().use("aui-debounce","liferay-autocomplete-input",(a=>{var o=t.path,i=[CKEDITOR.getUrl(o+"autocomplete.js")];CKEDITOR.scriptLoader.load(i,(()=>{var t=new Liferay.AutoCompleteCKEditor(a.merge(e.config.autocomplete,{editor:e,width:300}));t.render(),t.detach("valueChange")}))}))}});
+CKEDITOR.plugins.add("autocomplete",{init(e){var t=this;AUI().use("aui-debounce","liferay-autocomplete-input",(()=>{var a=t.path,o=[CKEDITOR.getUrl(a+"autocomplete.js")];CKEDITOR.scriptLoader.load(o,(()=>{var t=new Liferay.AutoCompleteCKEditor(Object.assign(e.config.autocomplete,{editor:e,width:300}));t.render(),t.detach("valueChange")}))}))}});
 //# sourceMappingURL=plugin.js.map
