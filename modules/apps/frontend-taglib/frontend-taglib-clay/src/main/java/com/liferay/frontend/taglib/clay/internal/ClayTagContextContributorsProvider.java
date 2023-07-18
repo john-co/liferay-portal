3fa8663337e9b1acad59e4a8f36cb5291c333e6f
John Co <john.co@liferay.com>
2023-07-18 15:01:49 -0700

    LPS-190770 TEST DO NOT COMMIT

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/ClayTagContextContributorsProvider.java
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/ClayTagContextContributorsProvider.java
@@ -9,7 +9,7 @@
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * details. TEST
  */
 
 package com.liferay.frontend.taglib.clay.internal;
