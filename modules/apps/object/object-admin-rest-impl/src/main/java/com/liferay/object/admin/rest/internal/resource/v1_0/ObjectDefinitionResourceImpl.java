6070321eb247089769ae77fe22de8ecd73757919
Brian Chan <brian.chan@liferay.com>
2023-11-30 15:56:53 -0300

    LPS-172418 Rename

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -71,7 +71,6 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Localization;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -88,10 +87,8 @@ import com.liferay.portal.vulcan.util.SearchUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
@@ -784,7 +781,7 @@ public class ObjectDefinitionResourceImpl
 			}
 		}
 
-		// Object Relationship must be created before Object Layout
+		// Object relationship must be created before object layout
 
 		if (objectRelationships != null) {
 			ObjectRelationshipResource.Builder builder =
@@ -795,8 +792,7 @@ public class ObjectDefinitionResourceImpl
 					contextUser
 				).build();
 
-			Map<String, Boolean> updateReverseObjectRelationshipMap =
-				new HashMap<>();
+			Set<String> updateReverseObjectRelationshipNames = new HashSet<>();
 
 			for (ObjectRelationship objectRelationship : objectRelationships) {
 				com.liferay.object.model.ObjectRelationship
@@ -816,8 +812,7 @@ public class ObjectDefinitionResourceImpl
 				}
 
 				if (serviceBuilderObjectRelationship != null) {
-					if (MapUtil.getBoolean(
-							updateReverseObjectRelationshipMap,
+					if (updateReverseObjectRelationshipNames.contains(
 							serviceBuilderObjectRelationship.getName())) {
 
 						serviceBuilderObjectRelationship =
@@ -836,8 +831,8 @@ public class ObjectDefinitionResourceImpl
 							ObjectRelationshipConstants.TYPE_MANY_TO_MANY) &&
 						serviceBuilderObjectRelationship.isSelf()) {
 
-						updateReverseObjectRelationshipMap.putIfAbsent(
-							serviceBuilderObjectRelationship.getName(), true);
+						updateReverseObjectRelationshipNames.add(
+							serviceBuilderObjectRelationship.getName());
 					}
 
 					continue;
@@ -855,8 +850,8 @@ public class ObjectDefinitionResourceImpl
 						objectRelationship.getObjectDefinitionId1(),
 						objectRelationship.getObjectDefinitionId2())) {
 
-					updateReverseObjectRelationshipMap.putIfAbsent(
-						objectRelationship.getName(), true);
+					updateReverseObjectRelationshipNames.add(
+						objectRelationship.getName());
 				}
 
 				if (accountEntryRestrictedObjectRelationshipsNames.contains(
