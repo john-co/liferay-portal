74fb7bc40cb2f83c9ed791423fb7a6335087adf6
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-12-04 16:53:37 +0100

    LPS-202532 External reference must be scoped by object definition
    I am essentially restoring the original logic, just keeping the new
    exception.
    
    The new logic was not compatible as the uniqueness of the ERC was being
    checked by group instead of by object definition.
    
    With this change we keep the same logic just using the right exception.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -274,15 +274,16 @@ public class ObjectEntryLocalServiceImpl
 
 		ObjectEntry objectEntry = objectEntryPersistence.create(objectEntryId);
 
+
 		objectEntry.setGroupId(groupId);
 		objectEntry.setCompanyId(user.getCompanyId());
-
-		_setExternalReferenceCode(objectEntry, values);
-
 		objectEntry.setUserId(user.getUserId());
 		objectEntry.setUserName(user.getFullName());
 		objectEntry.setCreateDate(new Date());
 		objectEntry.setObjectDefinitionId(objectDefinitionId);
+
+		_setExternalReferenceCode(objectEntry, values);
+
 		objectEntry.setStatus(WorkflowConstants.STATUS_DRAFT);
 		objectEntry.setStatusByUserId(user.getUserId());
 		objectEntry.setStatusDate(serviceContext.getModifiedDate(null));
@@ -3776,8 +3777,8 @@ public class ObjectEntryLocalServiceImpl
 
 				_validateExternalReferenceCode(
 					externalReferenceCode, objectEntry.getCompanyId(),
-					objectEntry.getGroupId(), objectEntry.getObjectEntryId(),
-					objectEntry.isNew());
+					objectEntry.getObjectDefinitionId(),
+					objectEntry.getObjectEntryId());
 
 				objectEntry.setExternalReferenceCode(externalReferenceCode);
 			}
@@ -3961,17 +3962,14 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	private void _validateExternalReferenceCode(
-		String externalReferenceCode, long companyId, long groupId,
-		long objectEntryId, boolean newObjectEntry) {
+		String externalReferenceCode, long companyId, long objectDefinitionId,
+		long objectEntryId) {
 
-		ObjectEntry ercObjectEntries = objectEntryPersistence.fetchByERC_G_C(
-			externalReferenceCode, groupId, companyId);
+		ObjectEntry objectEntry = objectEntryPersistence.fetchByERC_C_ODI(
+			externalReferenceCode, companyId, objectDefinitionId);
 
-		if (newObjectEntry && (ercObjectEntries != null)) {
-			throw new DuplicateObjectEntryExternalReferenceCodeException();
-		}
-		else if ((ercObjectEntries != null) &&
-				 (ercObjectEntries.getObjectEntryId() != objectEntryId)) {
+		if ((objectEntry != null) &&
+			(objectEntry.getObjectEntryId() != objectEntryId)) {
 
 			throw new DuplicateObjectEntryExternalReferenceCodeException();
 		}
