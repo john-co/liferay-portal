fc26300753e98668cfa19111c9e4f2fddada45a5
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-12-04 17:20:06 +0100

    LPS-202532 Setting error message

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3971,7 +3971,11 @@ public class ObjectEntryLocalServiceImpl
 		if ((objectEntry != null) &&
 			(objectEntry.getObjectEntryId() != objectEntryId)) {
 
-			throw new DuplicateObjectEntryExternalReferenceCodeException();
+			throw new DuplicateObjectEntryExternalReferenceCodeException(
+				StringBundler.concat(
+					"Duplicate object entry with external reference code ",
+					externalReferenceCode, " and object definition id ",
+					objectDefinitionId));
 		}
 	}
 
