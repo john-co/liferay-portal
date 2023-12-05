91a4af4fd987d4e2f455f27ad3f3c7e6c3273c43
Iván Zaera Avellón <ivan.zaera@liferay.com>
2023-12-05 14:02:49 +0100

    LPS-200229 Ignore multiple / inside paths in Combo Servlet

--- portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
@@ -265,6 +265,8 @@ public class ComboServletTest {
 
 		_testService("/js/aui.js", "/js/./aui.js", _portalServletContext);
 
+		_testService("/js/aui.js", "/js//aui.js", _portalServletContext);
+
 		_testService("/js/aui.js", "/js/down/../aui.js", _portalServletContext);
 	}
 
