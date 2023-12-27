9fb421347339ac7acdeaf3faa839a9eb5e8270f3
Pedro Leite <pedro.leite@liferay.com>
2023-12-27 10:26:03 -0300

    LPS-204581 Upgrade Process

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -21,6 +21,7 @@ import com.liferay.object.internal.upgrade.v3_27_0.ObjectActionUpgradeProcess;
 import com.liferay.object.internal.upgrade.v3_3_0.util.ObjectViewFilterColumnTable;
 import com.liferay.object.internal.upgrade.v3_9_0.ObjectLayoutBoxUpgradeProcess;
 import com.liferay.object.internal.upgrade.v6_0_0.util.ObjectValidationRuleSettingTable;
+import com.liferay.object.internal.upgrade.v8_6_2.SchemaUpgradeProcess;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.upgrade.BaseExternalReferenceCodeUpgradeProcess;
@@ -322,9 +323,11 @@ public class ObjectServiceUpgradeStepRegistrator
 				"ObjectDefinition", "storageType", "VARCHAR(255) null"),
 			UpgradeProcessFactory.alterColumnType(
 				"ObjectValidationRule", "engine", "VARCHAR(255) null"));
+		
+		registry.register("7.1.1", "7.1.2", new DummyUpgradeStep());
 
 		registry.register(
-			"7.1.1", "8.0.0",
+			"7.1.2", "8.0.0",
 			new com.liferay.object.internal.upgrade.v8_0_0.
 				ObjectFolderItemUpgradeProcess());
 
@@ -367,6 +370,8 @@ public class ObjectServiceUpgradeStepRegistrator
 				ObjectActionUpgradeProcess());
 
 		registry.register("8.6.0", "8.6.1", new DummyUpgradeStep());
+		
+		registry.register("8.6.1", "8.6.2", new SchemaUpgradeProcess());
 	}
 
 	@Reference
