c6ac842d8d2f92344557966a147f8995c665a925
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2024-01-03 12:03:04 -0300

    LPS-204716 Create constants for notification freemarker template

---
+++ modules/apps/notification/notification-api/src/main/java/com/liferay/notification/constants/NotificationFreemarkerTemplateConstants.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.notification.constants;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+public class NotificationFreemarkerTemplateConstants {
+
+	public static final String KEY_PORTAL_URL = "portal-url";
+
+	public static final String VARIABLE_NAME_PORTAL_URL = "portalURL";
+
+}
