7e4e89942901fd7e1a3b8d18a56fee41f4340dd1
Sandro Chinea <sandrochinea@gmail.com>
2024-01-09 11:51:36 +0000

    LPS-205105 Disable the node only if it has no children

--- modules/apps/layout/layout-taglib/src/main/resources/META-INF/resources/select_layout/js/SelectLayoutTree.js
+++ modules/apps/layout/layout-taglib/src/main/resources/META-INF/resources/select_layout/js/SelectLayoutTree.js
@@ -289,7 +289,9 @@ export function SelectLayoutTree({
 							<ClayTreeView.Group items={item.children}>
 								{(item) => (
 									<ClayTreeView.Item
-										disabled={item.disabled}
+										disabled={
+											item.disabled && !item.hasChildren
+										}
 										expandable={item.hasChildren}
 										onClick={(event) =>
 											onClick(event, item, selection)
