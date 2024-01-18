5ee7c2894c84bfc5b4fcb1fcd10d06cf0693ffe9
MarinhoFeliphe <feliphemarinho18@gmail.com>
2024-01-18 10:53:09 -0300

    LPS-206133 Reuse postObjectDefinition variable

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionResourceTest.java
@@ -315,12 +315,11 @@ public class ObjectDefinitionResourceTest
 
 		// Publish a persisted draft custom object definition
 
-		ObjectDefinition randomPersistedDraftObjectDefinition =
-			objectDefinitionResource.postObjectDefinition(
-				randomObjectDefinition());
+		postObjectDefinition = objectDefinitionResource.postObjectDefinition(
+			randomObjectDefinition());
 
 		Assert.assertEquals(
-			randomPersistedDraftObjectDefinition.getStatus(),
+			postObjectDefinition.getStatus(),
 			new Status() {
 				{
 					code = WorkflowConstants.STATUS_DRAFT;
@@ -334,7 +333,7 @@ public class ObjectDefinitionResourceTest
 				}
 			});
 
-		randomPersistedDraftObjectDefinition.setStatus(
+		postObjectDefinition.setStatus(
 			new Status() {
 				{
 					code = WorkflowConstants.STATUS_APPROVED;
@@ -343,8 +342,7 @@ public class ObjectDefinitionResourceTest
 
 		ObjectDefinition randomPersistedPublishedObjectDefinition =
 			objectDefinitionResource.putObjectDefinition(
-				randomPersistedDraftObjectDefinition.getId(),
-				randomPersistedDraftObjectDefinition);
+				postObjectDefinition.getId(), postObjectDefinition);
 
 		Assert.assertEquals(
 			randomPersistedPublishedObjectDefinition.getStatus(),
