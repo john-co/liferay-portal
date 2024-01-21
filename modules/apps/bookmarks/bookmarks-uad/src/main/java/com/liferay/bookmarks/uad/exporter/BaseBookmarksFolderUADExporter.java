2bf2ac2bdeb78ff0d40dd240ca820f136b4c7397
Stefano Motta <117469730+smottal@users.noreply.github.com>
2024-01-21 17:47:12 +0100

    LPS-199049 - bookmarks-uad - Autogenerated Code

--- modules/apps/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/exporter/BaseBookmarksFolderUADExporter.java
+++ modules/apps/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/exporter/BaseBookmarksFolderUADExporter.java
@@ -54,11 +54,11 @@ public abstract class BaseBookmarksFolderUADExporter
 
 		sb.append(
 			"<column><column-name>folderId</column-name><column-value><![CDATA[");
-		sb.append(bookmarksFolder.getFolderId());
+		sb.append("0");
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
-		sb.append(bookmarksFolder.getStatusByUserId());
+		sb.append("0");
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
@@ -66,7 +66,7 @@ public abstract class BaseBookmarksFolderUADExporter
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(bookmarksFolder.getUserId());
+		sb.append("0");
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>userName</column-name><column-value><![CDATA[");
