8fcb2c1b9141e10a3070459c33dcd95c07cd8f29
Stefano Motta <117469730+smottal@users.noreply.github.com>
2024-01-21 17:52:00 +0100

    LPS-199049 - site-teams-uad - Autogenerated Code

--- modules/apps/site/site-teams-uad/src/main/java/com/liferay/site/teams/uad/exporter/BaseTeamUADExporter.java
+++ modules/apps/site/site-teams-uad/src/main/java/com/liferay/site/teams/uad/exporter/BaseTeamUADExporter.java
@@ -54,11 +54,11 @@ public abstract class BaseTeamUADExporter
 
 		sb.append(
 			"<column><column-name>teamId</column-name><column-value><![CDATA[");
-		sb.append(team.getTeamId());
+		sb.append("0");
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(team.getUserId());
+		sb.append("0");
 		sb.append("]]></column-value></column>");
 		sb.append(
 			"<column><column-name>userName</column-name><column-value><![CDATA[");
