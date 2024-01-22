2399e6343a282bc777978775b7a06782445c9129
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-01-22 15:56:03 +0100

    LPD-1466 Only update asset entry if we get a higher version of the article

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
@@ -1104,11 +1104,18 @@ public class JournalArticleStagedModelDataHandler
 					serviceContext);
 			}
 
-			_journalArticleLocalService.updateAsset(
-				userId, importedArticle, serviceContext.getAssetCategoryIds(),
-				serviceContext.getAssetTagNames(),
-				serviceContext.getAssetLinkEntryIds(),
-				serviceContext.getAssetPriority());
+			if (_updateAsset(
+					importedArticle.getGroupId(),
+					importedArticle.getArticleId(),
+					importedArticle.getVersion())) {
+
+				_journalArticleLocalService.updateAsset(
+					userId, importedArticle,
+					serviceContext.getAssetCategoryIds(),
+					serviceContext.getAssetTagNames(),
+					serviceContext.getAssetLinkEntryIds(),
+					serviceContext.getAssetPriority());
+			}
 
 			if (article.isExpired() || importedArticle.isExpired()) {
 				_journalArticleLocalService.expireArticle(
@@ -1749,6 +1756,19 @@ public class JournalArticleStagedModelDataHandler
 				_journalArticleLocalService.updateJournalArticle(curArticle);
 			}
 		}
+	}
+
+	private boolean _updateAsset(
+		long groupId, String articleId, double version) {
+
+		JournalArticle article = _journalArticleLocalService.fetchLatestArticle(
+			groupId, articleId, WorkflowConstants.STATUS_APPROVED);
+
+		if ((article == null) || (version >= article.getVersion())) {
+			return true;
+		}
+
+		return false;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
