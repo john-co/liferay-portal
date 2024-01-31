81859ef1a20a63a9e51696d832f4b42921237637
Carlos Correa <carlos.correa@liferay.com>
2024-01-31 17:52:25 +0100

    LPD-15236 Update 'entity.class.name' value for Object Entry REST API, as for object definitions (in diff companies) with a similar name (for example, Test and test), the JAX-RS Resource is the same, so we need to use a name that could be calculated from both objects, that is a lowercase name

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
@@ -1622,7 +1622,8 @@ public class ObjectActionLocalServiceTest {
 					StringBundler.concat(
 						com.liferay.object.rest.dto.v1_0.ObjectEntry.class.
 							getName(),
-						StringPool.POUND, _objectDefinition.getName()));
+						StringPool.POUND,
+						StringUtil.toLowerCase(_objectDefinition.getName())));
 
 			objectEntryResource.setContextAcceptLanguage(
 				new AcceptLanguage() {
