d820d2664055e87928b0c2df9c46af7ac9bf5ab8
Carlos Correa <carlos.correa@liferay.com>
2024-01-31 16:46:58 +0100

    LPD-15236 Add entity.class.name as it is the property that is being used to determine the taskItemDelegateName parameter in Batch.
    See: https://github.com/liferay/liferay-portal/blob/6ce0b9eb049e99a50285a324a3bd4afd76d340ab/modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/batch/engine/VulcanBatchEngineTaskItemDelegateRegistryImpl.java#L164

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -388,6 +388,10 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 								"batch.planner.import.enabled", "true"
 							).put(
 								"companyId", objectDefinition.getCompanyId()
+							).put(
+								"entity.class.name",
+								ObjectEntry.class.getName() + "#" +
+									osgiJaxRsName
 							).build()),
 						_bundleContext.registerService(
 							ObjectRelationshipElementsParser.class,
