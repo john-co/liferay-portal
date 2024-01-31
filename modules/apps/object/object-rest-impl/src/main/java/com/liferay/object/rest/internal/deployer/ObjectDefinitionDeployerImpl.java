ad12379c1512fa4292f86353fbe4d9fbcf2eda96
Carlos Correa <carlos.correa@liferay.com>
2024-01-31 16:56:55 +0100

    LPD-15236 Fix usage of osgiJaxRsName. Its purpose is for JAX-RS components, for the rest of properties better use object definition name

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/deployer/ObjectDefinitionDeployerImpl.java
@@ -380,7 +380,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 							HashMapDictionaryBuilder.<String, Object>put(
 								"batch.engine.entity.class.name",
 								ObjectEntry.class.getName() + "#" +
-									osgiJaxRsName
+									objectDefinition.getName()
 							).put(
 								"batch.engine.task.item.delegate", "true"
 							).put(
@@ -395,7 +395,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 							).put(
 								"entity.class.name",
 								ObjectEntry.class.getName() + "#" +
-									osgiJaxRsName
+									objectDefinition.getName()
 							).build()),
 						_bundleContext.registerService(
 							ObjectRelationshipElementsParser.class,
@@ -421,7 +421,7 @@ public class ObjectDefinitionDeployerImpl implements ObjectDefinitionDeployer {
 			"companyId", companyIds
 		).put(
 			"entity.class.name",
-			ObjectEntry.class.getName() + "#" + osgiJaxRsName
+			ObjectEntry.class.getName() + "#" + objectDefinition.getName()
 		).put(
 			"osgi.jaxrs.application.select",
 			"(osgi.jaxrs.name=" + osgiJaxRsName + ")"
