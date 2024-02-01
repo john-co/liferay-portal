2c64e13f2d422755353f63292e6b0150b4ae7264
Brian I. Kim <il.kim@liferay.com>
2024-02-01 14:30:03 -0800

    LPD-15573 Support WebDAVPassword generation in UsersAdminPortlet

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/action/GenerateWebDAVPasswordMVCActionCommand.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/action/GenerateWebDAVPasswordMVCActionCommand.java
@@ -26,6 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	property = {
 		"javax.portlet.name=" + UsersAdminPortletKeys.MY_ACCOUNT,
+		"javax.portlet.name=" + UsersAdminPortletKeys.USERS_ADMIN,
 		"mvc.command.name=/users_admin/generate_webdav_password"
 	},
 	service = MVCActionCommand.class
