3ed3528cce123abd51594ba1404d5be4c5ee7876
Danny Situ <danny.situ@liferay.com>
2024-02-01 09:37:21 -0800

    LPD-16328 Validate userIds being added to organizations

--- portal-impl/src/com/liferay/portal/service/impl/UserServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserServiceImpl.java
@@ -171,6 +171,8 @@ public class UserServiceImpl extends UserServiceBaseImpl {
 			return;
 		}
 
+		validateUserIds(userIds);
+
 		OrganizationPermissionUtil.check(
 			getPermissionChecker(), organizationId, ActionKeys.ASSIGN_MEMBERS);
 
@@ -3900,6 +3902,12 @@ public class UserServiceImpl extends UserServiceBaseImpl {
 
 		if (userFieldException.hasFields()) {
 			throw userFieldException;
+		}
+	}
+
+	protected void validateUserIds(long[] userIds) throws PortalException {
+		for (long userId : userIds) {
+			getUserById(userId);
 		}
 	}
 
