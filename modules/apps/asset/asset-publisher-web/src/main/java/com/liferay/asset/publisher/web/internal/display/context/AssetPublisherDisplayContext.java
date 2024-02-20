3b9e1890b7e4971ab72c7648fb80006ef018d24b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-02-20 08:06:06 +0000

    LPD-18178 Simplify

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -2230,19 +2230,16 @@ public class AssetPublisherDisplayContext {
 		List<AssetEntry> assetEntries =
 			(List<AssetEntry>)infoPage.getPageItems();
 
-		if (assetEntries.isEmpty() ||
-			(ArrayUtil.isEmpty(getAllAssetCategoryIds()) &&
-			 ArrayUtil.isEmpty(getAllAssetTagNames()))) {
+		if (!assetEntries.isEmpty() &&
+			!ArrayUtil.isEmpty(getAllAssetCategoryIds())) {
 
-			return assetEntries;
-		}
-
-		if (!ArrayUtil.isEmpty(getAllAssetCategoryIds())) {
 			assetEntries = _filterAssetCategoriesAssetEntries(
 				assetEntries, getAllAssetCategoryIds());
 		}
 
-		if (!ArrayUtil.isEmpty(getAllAssetTagNames())) {
+		if (!assetEntries.isEmpty() &&
+			!ArrayUtil.isEmpty(getAllAssetTagNames())) {
+
 			assetEntries = _filterAssetTagNamesAssetEntries(
 				assetEntries, getAllAssetTagNames());
 		}
