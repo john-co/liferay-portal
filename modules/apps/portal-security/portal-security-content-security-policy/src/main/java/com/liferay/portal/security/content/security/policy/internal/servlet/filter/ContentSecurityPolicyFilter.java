c8eec7641a276f3c1476fdddcbc593cc726e69f9
ChrisKian <christopher.kian@liferay.com>
2024-02-22 10:45:04 -0800

    LPS-206370 auto SF

--- modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilter.java
+++ modules/apps/portal-security/portal-security-content-security-policy/src/main/java/com/liferay/portal/security/content/security/policy/internal/servlet/filter/ContentSecurityPolicyFilter.java
@@ -115,68 +115,6 @@ public class ContentSecurityPolicyFilter extends BasePortalFilter {
 		httpServletResponse.setHeader("Content-Security-Policy", policy);
 	}
 
-	private String _updateContent(String content, String nonce) {
-		String nonceAttribute = "nonce=\"" + nonce + "\"";
-		String escapedNonceAttribute = "nonce=\\\"" + nonce + "\\\"";
-
-		content = content.replaceAll(
-			"<(?i)link ", "<link " + nonceAttribute + " ");
-		content = content.replaceAll(
-			"<(?i)link>", "<link " + nonceAttribute + ">");
-		content = content.replaceAll(
-			"<(?i)script ", "<script " + nonceAttribute + " ");
-		content = content.replaceAll(
-			"<(?i)script>", "<script " + nonceAttribute + ">");
-		content = content.replaceAll(
-			"<(?i)style ", "<style " + nonceAttribute + " ");
-		content = content.replaceAll(
-			"<(?i)style>", "<style " + nonceAttribute + ">");
-
-		Pattern pattern = Pattern.compile(
-			"\\{.*nonce=\".{" + nonce.length() + "}\".*\\}");
-
-		Matcher matcher = pattern.matcher(content);
-
-		while (matcher.find()) {
-			String matcherGroup = matcher.group();
-
-			String[] matcherArray = StringUtil.split(
-				matcherGroup, nonceAttribute);
-
-			StringBundler sb = new StringBundler((matcherArray.length * 2) - 1);
-
-			int open = 0;
-			boolean overwrite = false;
-
-			for (int i = 0; i < (matcherArray.length - 1); i++) {
-				open += StringUtil.count(
-					matcherArray[i], CharPool.OPEN_CURLY_BRACE);
-				open -= StringUtil.count(
-					matcherArray[i], CharPool.CLOSE_CURLY_BRACE);
-
-				sb.append(matcherArray[i]);
-
-				if (open > 0) {
-					overwrite = true;
-
-					sb.append(escapedNonceAttribute);
-				}
-				else {
-					sb.append(nonceAttribute);
-				}
-			}
-
-			if (overwrite) {
-				sb.append(matcherArray[matcherArray.length - 1]);
-
-				content = StringUtil.replace(
-					content, matcherGroup, sb.toString());
-			}
-		}
-
-		return content;
-	}
-
 	private String _generateNonce() {
 		SecureRandom secureRandom = new SecureRandom();
 
@@ -240,6 +178,68 @@ public class ContentSecurityPolicyFilter extends BasePortalFilter {
 		}
 
 		return false;
+	}
+
+	private String _updateContent(String content, String nonce) {
+		String nonceAttribute = "nonce=\"" + nonce + "\"";
+		String escapedNonceAttribute = "nonce=\\\"" + nonce + "\\\"";
+
+		content = content.replaceAll(
+			"<(?i)link ", "<link " + nonceAttribute + " ");
+		content = content.replaceAll(
+			"<(?i)link>", "<link " + nonceAttribute + ">");
+		content = content.replaceAll(
+			"<(?i)script ", "<script " + nonceAttribute + " ");
+		content = content.replaceAll(
+			"<(?i)script>", "<script " + nonceAttribute + ">");
+		content = content.replaceAll(
+			"<(?i)style ", "<style " + nonceAttribute + " ");
+		content = content.replaceAll(
+			"<(?i)style>", "<style " + nonceAttribute + ">");
+
+		Pattern pattern = Pattern.compile(
+			"\\{.*nonce=\".{" + nonce.length() + "}\".*\\}");
+
+		Matcher matcher = pattern.matcher(content);
+
+		while (matcher.find()) {
+			String matcherGroup = matcher.group();
+
+			String[] matcherArray = StringUtil.split(
+				matcherGroup, nonceAttribute);
+
+			StringBundler sb = new StringBundler((matcherArray.length * 2) - 1);
+
+			int open = 0;
+			boolean overwrite = false;
+
+			for (int i = 0; i < (matcherArray.length - 1); i++) {
+				open += StringUtil.count(
+					matcherArray[i], CharPool.OPEN_CURLY_BRACE);
+				open -= StringUtil.count(
+					matcherArray[i], CharPool.CLOSE_CURLY_BRACE);
+
+				sb.append(matcherArray[i]);
+
+				if (open > 0) {
+					overwrite = true;
+
+					sb.append(escapedNonceAttribute);
+				}
+				else {
+					sb.append(nonceAttribute);
+				}
+			}
+
+			if (overwrite) {
+				sb.append(matcherArray[matcherArray.length - 1]);
+
+				content = StringUtil.replace(
+					content, matcherGroup, sb.toString());
+			}
+		}
+
+		return content;
 	}
 
 	private static final String[] _INTERNALLY_EXCLUDED_PATHS = {
