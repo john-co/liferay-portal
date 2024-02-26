4e1e4f619778ad6b8b7b2cd427271e6952c5652b
Brian Chan <brian.chan@liferay.com>
2024-02-26 10:28:33 -0300

    LPD-17242 auto SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -746,6 +746,10 @@ public class DDMFormDisplayContext {
 		return false;
 	}
 
+	public boolean isPropagateLanguageSelection() {
+		return _ddmFormWebConfiguration.propagateLanguageSelection();
+	}
+
 	public boolean isRememberMe() {
 		String rememberMe = CookiesManagerUtil.getCookieValue(
 			CookiesConstants.NAME_REMEMBER_ME, _getHttpServletRequest());
@@ -851,10 +855,6 @@ public class DDMFormDisplayContext {
 			isSubmissionLimitReached(
 				getFormInstance(), _ddmFormInstanceRecordVersionLocalService,
 				getUser());
-	}
-
-	public boolean isPropagateLanguageSelection() {
-		return _ddmFormWebConfiguration.propagateLanguageSelection();
 	}
 
 	protected DDMFormRenderingContext createDDMFormRenderingContext(
