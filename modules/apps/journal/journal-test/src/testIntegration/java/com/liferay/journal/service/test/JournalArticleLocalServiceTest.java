a996236db54fbcad739c68ff3d39bb0da402d20c
Brian Chan <brian.chan@liferay.com>
2024-03-05 11:26:39 +0400

    LPD-19234 Match journal_content_with_multiple_images

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/test/JournalArticleLocalServiceTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/test/JournalArticleLocalServiceTest.java
@@ -408,7 +408,7 @@ public class JournalArticleLocalServiceTest {
 	}
 
 	@Test
-	public void testCopyArticleWithDuplicatedImages() throws Exception {
+	public void testCopyArticleWithMultipleImages() throws Exception {
 		FileEntry fileEntry = _dlAppLocalService.addFileEntry(
 			null, TestPropsValues.getUserId(), _group.getGroupId(),
 			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
