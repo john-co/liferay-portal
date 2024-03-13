c589562315385278f19cdcf99f9a3db3026c248f
John Co <john.co@liferay.com>
2024-03-13 12:28:12 -0700

    LPS-test Implement a working test

--- modules/test/playwright/tests/clay-sample-web/alertToast.spec.ts
+++ modules/test/playwright/tests/clay-sample-web/alertToast.spec.ts
@@ -5,77 +5,45 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
-import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {loginTest} from '../../fixtures/loginTest';
-import {widgetPagesTest} from '../../fixtures/widgetPagesTest';
-import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
-import getRandomString from '../../utils/getRandomString';
-import {pageEditorPagesTest} from '../layout-content-page-editor-web/fixtures/pageEditorPagesTest';
-
-// export const test = mergeTests(
-// 	apiHelpersTest,
-// 	applicationsMenuPageTest,
-// 	featureFlagsTest({
-// 		'LPS-178052': true,
-// 	}),
-// 	isolatedSiteTest,
-// 	loginTest(),
-// 	pageEditorPagesTest
-// );
-
 
 export const test = mergeTests(
-	apiHelpersTest,
-	isolatedSiteTest,
-	loginTest(),
-	pageEditorPagesTest,
-	widgetPagesTest
+	loginTest()
 );
 
 test.describe('Clay Alert Toast', () => {
 	test('LRQA-65932 Validate alert toast message does not disapear when mouse hovers over it.', async({
-		apiHelpers,
 		page,
-		site,
-		pageEditorPage
 	}) => {
 		await test.step('Given clay sample portlet', async () => {
 			await page.goto('/');
-
-			// Create a page with a Heading fragment
-
-			const headingId = getRandomString();
-
-			const layout = await apiHelpers.headlessDelivery.createSitePage(
-				site.id,
-				getRandomString()
-			);
-
-			// Add sample page
-			// Add widget to page
-			// column 1
-
-			// Go to edit mode of page
-
-			await pageEditorPage.goToEditMode(site, layout);
-
-			// Assert undo button is disabled
-
-			await expect(pageEditorPage.undoButton).toBeDisabled();
-
+			await page.getByRole('link', { name: 'Edit' }).click();
+			await page.getByLabel('Search Fragments and Widgets').click();
+			await page.getByLabel('Search Fragments and Widgets').fill('clay sample');
+			await page.getByRole('menuitem', { name: 'Clay Sample Add Clay Sample Mark Clay Sample as Favorite' }).click();
+			await page.keyboard.press('Tab');
+			await expect(page.getByLabel('Add Clay Sample')).toBeFocused();
+			await page.keyboard.press('Enter');
+			await page.keyboard.press('Enter');
+			await expect(page.locator('header').filter({ hasText: 'Clay Sample' })).toBeVisible();
+			await page.getByLabel('Publish').click();
 		});
 
 		await test.step('When a toast alert appears', async () => {
 			// Click on fail button
+			await page.getByRole('button', { name: 'Fail Submit' }).click();
 		});
 
 		await test.step('And user mouses over the alert', async () => {
 			// Make mouse hover over the alert
+  			await page.getByText('Error:An unexpected error occurred.').hover();
 		});
 
 		await test.step('Then the alert does not disappear when hovered over', async () => {
-			// Pause for 20 seconds?
+			// Pause for 20 seconds
+			await page.waitForTimeout(20000);
 			// Assert alert message is present
+			await expect(page.getByText('Error:An unexpected error occurred.')).toBeVisible();
 		});
 	});
 });
